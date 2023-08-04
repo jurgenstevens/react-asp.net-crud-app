35fa1787b7b87d3030150c2ca1cb80aa08ca0892
jurgenstevens <jurgen.stevens@yahoo.com>
2023-08-04 00:00:10 -0500

    Import Mocrosoft.EntityFramework.

--- Data/AppDBContext.cs
+++ Data/AppDBContext.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 namespace aspnetserver.Data
 {
 	public class AppDBContext
 	{
 		public AppDBContext()
 		{
+			public Microsoft.EntityFrameworkCore.DbSet<Post> Posts { get; set; }
+			protected override void OnConfiguring(DbContext)
 		}
 	}
 }
