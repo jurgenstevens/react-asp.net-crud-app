59f2be87e15d871823de9bbc3f3e3955e55a131f
jurgenstevens <jurgen.stevens@yahoo.com>
2023-08-09 23:52:33 -0500

    Seed post data to local sqlite db.

--- Data/AppDBContext.cs
+++ Data/AppDBContext.cs
@@ -7,6 +7,22 @@ namespace aspnetserver.Data
         public DbSet<Post> Posts { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder dbContextOptionsBuilder) => dbContextOptionsBuilder.UseSqlite("Data Source=./Data/AppDB.db");
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            Post[] postsToSeed = new Post[6];
+
+            for (int i = 1; i <= 6; i++)
+            {
+                postsToSeed[i - 1] = new Post
+                {
+                    PostId = i,
+                    Title = $"Post {i}",
+                    Content = $"{i}) Hello World!"
+                };
+            }
+
+            modelBuilder.Entity<Post>().HasData(postsToSeed);
+        }
     }
 }
-
